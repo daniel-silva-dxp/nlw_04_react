a635ce50b93215a4841e32d426105b3e5e87d4bf
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-09 18:42:25 -0300

    feat: entendendo components, estado e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello NLW</h1>
+      <Button color="red">Button One</Button>
+      <Button color="green">Button Two</Button>
+      <Button color="blue">Button Three</Button>
     </div>
   );
 }
