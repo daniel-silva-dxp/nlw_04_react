b8b615b4e80be353b925dee5672672ab24bacd3a
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-10 18:58:02 -0300

    feat: finalizando component profile

--- src/components/Perfile.tsx
+++ src/components/Perfile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/daniel-silva-dxp.png" alt="Profile image" />
       <div>
         <strong>Daniel Silva</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
